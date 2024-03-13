21510d1d8f7b7a9a50b4a6445431e665abd8c595
Adam Robson <adamray312@gmail.com>
2024-03-13 00:15:07 -0700

    add initial build of sign in sign out functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 export default function App() {
+  const session = useSession();
+  const client = useSupabaseClient();
+
+   const googleSignIn = async (): Promise<void> => {
+    const { error } = await client.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        scopes: "https://www.googleapis.com/auth/calendar"
+      }
+    });
+
+    if (error) {
+      console.log('Error signing in to Google provider with Supabase: ', error);
+    }
+  }
+
+  async function signOut() {
+    await client.auth.signOut();
+  }
+
   return (
-    <div>App</div>
+    <div>
+      {session ? (
+        <div>
+          <span>Signed in as: {session.user?.email}</span>
+          <button onClick={signOut}>Sign out</button>
+       </div>
+      ) : (
+        <div>
+          <span>Sign in</span>
+            <button onClick={googleSignIn}>Sign in with Google</button>
+        </div>
+      )}
+    </div>
   )
 }
